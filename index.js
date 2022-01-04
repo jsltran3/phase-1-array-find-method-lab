e2f26135acbd744e00333241b40c92c31a1b86c0
Joseph <jsltran@gmail.com>
2022-01-04 14:24:40 -0800

    commit tree d7cea23bece18943f6ffa3fec6955f0185703eb4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// function superbowlWin(winRecord) {
+//     return winRecord.result === 'W'
+//   }
+  
+//   let yearWon = record.find(superbowlWin);
+
+function superbowlWin(arr) {
+    let winResult = arr.find(winRecord => winRecord.result === 'W');
+    if (winResult === undefined) {
+        return undefined
+    } else {
+        return winResult.year; 
+    }
+}
+  
+// function superbowlWin(arr) {
+//     if (arr.find(winRecord => winRecord.result === 'W') ){
+//         return arr.year;
+//     } else {
+//         return "undefined"
+//     }
+// }
+
